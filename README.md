7cdd99ad858b9313cb4a248704ce66367faa8f72
Oleksandr Vashchenko <oleksandr.vashchenko@sap.com>
2022-11-24 14:30:17 -0600

    init commit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ How to launch Exchange rate application:
 
 1. Start the Spring Boot Application
 2. Query http://localhost:8080/rates/usd/uah
-3. Or use src/main/java/top/desq/test.http file
+3. Or use test.http file
 
 Requirements:
 - Java 17
--- src/main/java/top/desq/controller/ExchangeController.java
+++ src/main/java/top/desq/controller/ExchangeController.java
@@ -1,6 +1,7 @@
 package top.desq.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import top.desq.util.TimeUtil;
 @RestController
 @RequestMapping("/rates")
 @RequiredArgsConstructor
+@Slf4j
 public class ExchangeController {
 
     private final ExchangeService rateService;
@@ -21,13 +23,13 @@ public class ExchangeController {
     public ResponseEntity<ResponseDto> showRates() {
         var current = rateService.getCurrentRate();
         var average = rateService.getAverageRate();
-        var msg = "Текущий курс: %s. Средний курс: %s. Дата: %s"
-                .formatted(current, average, TimeUtil.getFormattedDate());
+        var msg = "Текущий курс: %s. Средний курс: %s. Дата: %s".formatted(current, average, TimeUtil.getFormattedDate());
         return ResponseEntity.ok(new ResponseDto("success", msg));
     }
 
-    @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<ResponseDto> handleException(RuntimeException e) {
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ResponseDto> handleException(Exception e) {
+        log.info("Exception thrown: {}", e.getMessage());
         return ResponseEntity.ok(new ResponseDto("error", e.getMessage()));
     }
 }
